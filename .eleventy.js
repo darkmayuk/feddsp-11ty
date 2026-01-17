8dacba5920425a62d140157d9306e30ddeec8a1e
Mark Day <mark@markday.co.uk>
2026-01-17 18:54:47 +0000

    added eleventy-img and refactored

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,34 @@
+const Image = require("@11ty/eleventy-img");
+const path = require("path");
+
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/assets");
+  eleventyConfig.addAsyncShortcode("image", async function(src, alt, sizes = "100vw", className = "") {
+    
+    let fullSrc = src;
+    if(src.startsWith("/")) {
+        fullSrc = "src" + src; 
+    }
+
+    let metadata = await Image(fullSrc, {
+      widths: [600, 900, 1500, "auto"], // "auto" keeps original width
+      formats: ["webp", "jpeg"],        // webp for modern, jpeg for fallback
+      outputDir: "./dist/assets/img-opt/", // Write optimized files here
+      urlPath: "/assets/img-opt/",         // The path in the HTML
+      
+    });
+
+    let imageAttributes = {
+      alt,
+      sizes,
+      loading: "lazy",
+      decoding: "async",
+      class: className
+    };
+
+    return Image.generateHTML(metadata, imageAttributes);
+  });
+
   return {
     dir: {
       input: "src",
